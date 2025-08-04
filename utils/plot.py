e9e4d13c89540142169afdb08ef145f8665248aa
jcarlossc <josecscosta@fac.pe.senac.br>
2025-08-04 19:48:36 -0300

    Módulo plot adicionado.

---
+++ utils/plot.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+def plotar_graficos(x, y, titulo, xlabel, ylabel, nome_arquivo):
+    plt.figure(figsize=(12, 4))
+    plt.plot(x, y, marker='o', linestyle='-', color='tab:blue')
+    plt.title(titulo)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.grid(True)
+    plt.tight_layout()
+    plt.savefig(f"data/{nome_arquivo}")
+    plt.close()
